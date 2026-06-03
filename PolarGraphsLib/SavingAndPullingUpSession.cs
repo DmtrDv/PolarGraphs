d1c40a68b35387c52a1072e213766cf94b00ab90
DmtrDv <dmitriybychkov75146@gmail.com>
2026-06-03 21:27:11 +0300

    создан метод загрузки сессии

--- PolarGraphsLib/SavingAndPullingUpSession.cs
+++ PolarGraphsLib/SavingAndPullingUpSession.cs
@@ -9,28 +9,26 @@ namespace PolarGraphsLib
 {
     public class SavingAndPullingUpSession
     {
-        static string pathToSession_ = "Session\\Session.txt";
-        /* public static List<PolarFunction> ReadPolarFunction()
+         static string pathToSession_ = "Session\\Session.txt";
+         public static (PolarFunction, bool, bool) PullingUpSession()
          {
-             List<PolarFunction> listFunctions = new List<PolarFunction>();
-             string textFunctions = File.ReadAllText(pathToSession_);
-             string[] arrayFunctions = textFunctions.Split('\n');
+             string textSession = File.ReadAllText(pathToSession_);
+             string[] arraySession = textSession.Split('\n');
 
-             for (int i = 0; i < arrayFunctions.Length; i++)
+             string[] function = arraySession[0].Split(';');
+             PolarFunction polarFunctionFromLastSession = new PolarFunction()
              {
-                 string[] function = arrayFunctions[i].Split(';');
-                 listFunctions.Add(new PolarFunction
-                 {
-                     Id = int.Parse(function[0]),
-                     Name = function[1],
-                     Function = function[2],
-                     Step = Convert.ToDouble(function[3]),
-                     StartCorner = Convert.ToDouble(function[4]),
-                     EndCorner = Convert.ToDouble(function[5])
-                 });
-             }
-             return listFunctions;
-         }*/
+                 Id = -1,
+                 Name = function[1],
+                 Function = function[2],
+                 Step = Convert.ToDouble(function[3]),
+                 StartCorner = Convert.ToDouble(function[4]),
+                 EndCorner = Convert.ToDouble(function[5])
+             };
+             bool boolListPoints = Convert.ToBoolean(arraySession[1]);
+             bool darkMode = Convert.ToBoolean(arraySession[2]);
+             return (polarFunctionFromLastSession, boolListPoints, darkMode);
+         }
         public static void SaveSession(PolarFunction polarFunction, bool boolListPoints)
         {
             string recordable = null;
